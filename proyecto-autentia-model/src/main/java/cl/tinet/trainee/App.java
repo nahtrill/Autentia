36a91906ba29b6a1c02927e395eb43c4a6375460
nahtrill <camilo.arriagada@alumnos.usm.cl>
2016-02-19 17:36:19 -0300

    Autentia
    Ts. Proyecto Autentia

---
+++ proyecto-autentia-model/src/main/java/cl/tinet/trainee/App.java
@@ -0,0 +1,13 @@
+package cl.tinet.trainee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
